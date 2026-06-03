6fbf600ababafcbfc525e17386802659ff6a6ba2
Ahmad <monyeran6377@gmail.com>
2026-06-03 19:57:10 +0530

    Remove FloodWait handler from main.py
    Removed the message handler function that dealt with FloodWait errors.

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 # Don't Remove Credit Tg - @newstudent1885
 # Ask Doubt on telegram @newstudent1885
 import time
-from pyrogram.errors import FloodWait
-
-@app.on_message()
-def handler(client, message):
-    try:
-        client.send_message(message.chat.id, "Hello!")
-    except FloodWait as e:
-        print(f"FloodWait: waiting {e.value} seconds")
-        time.sleep(e.value)
-        client.send_message(message.chat.id, "Hello after wait!")
-
+from pyrogram.errors import FloodWai
 import os
 import re
 import sys
